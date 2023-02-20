2ba59f5f28eb2e1785f77da3076338b80dec9c0f
Anna Artemova <annartmva@gmail.com>
2023-02-20 21:23:03 +0400

    Add deploy settings

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+set -e
+npm run build
+cd dist
+echo > .nojekyll
+
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:annaindistress/frontend-mentor-todo-app.git main:gh-pages
+
+cd -
