bf76937cc7455341cbd4680668d8f66476088576
Anna Artemova <annartmva@gmail.com>
2023-02-23 00:13:32 +0400

    Add header and switch between themes

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,51 @@
 'use strict';
+
+const themeToggle = document.querySelector('.header__theme-toggle');
+const darkThemeStyles = document.head.querySelector(
+  'link[rel=stylesheet][media*=prefers-color-scheme][media*=dark]'
+);
+
+class App {
+  #colorTheme;
+
+  constructor() {
+    this._getCurrentTheme();
+    this._applyTheme();
+
+    themeToggle.addEventListener('click', this._changeTheme.bind(this));
+  }
+
+  _applyTheme() {
+    const darkThemeMediaMap = {
+      auto: '(prefers-color-scheme: dark)',
+      light: 'not all',
+      dark: 'all',
+    };
+    darkThemeStyles.media = darkThemeMediaMap[this.#colorTheme];
+  }
+
+  _changeTheme() {
+    let newTheme;
+    if (this.#colorTheme === 'auto') {
+      newTheme = window.matchMedia('(prefers-color-scheme: dark)')
+        ? 'light'
+        : 'dark';
+    } else {
+      newTheme = this.#colorTheme === 'dark' ? 'light' : 'dark';
+    }
+    this.#colorTheme = newTheme;
+    this._setCurrentTheme();
+    this._applyTheme();
+  }
+
+  _setCurrentTheme() {
+    localStorage.setItem('color-theme', this.#colorTheme);
+  }
+
+  _getCurrentTheme() {
+    const data = localStorage.getItem('color-theme');
+    this.#colorTheme = data || 'auto';
+  }
+}
+
+const app = new App();
